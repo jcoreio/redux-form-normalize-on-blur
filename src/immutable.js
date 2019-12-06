33f0600a77a6cd383cf7cb1ea6859c4e525e4066
Andy Edwards <jedwards@fastmail.com>
2019-12-05 20:45:24 -0600

    fix: more flow type issues

--- src/immutable.js
+++ src/immutable.js
@@ -2,10 +2,9 @@
 
 import * as React from 'react'
 import { Field as _Field } from 'redux-form/immutable'
+import type { Props as FieldInputProps } from 'redux-form/lib/FieldProps.types'
 import createNormalizeOnBlurField from './createNormalizeOnBlurField'
 
-const Field = createNormalizeOnBlurField<React.ElementConfig<typeof _Field>>(
-  _Field
-)
+const Field = createNormalizeOnBlurField<FieldInputProps>(_Field)
 
 export { Field, createNormalizeOnBlurField }
